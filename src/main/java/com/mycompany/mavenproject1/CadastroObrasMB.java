0702c13df8e225c6df8c5ec053a692f7282d5cbc
Kamila <Kamila@Wolverine-PC>
2015-02-19 15:02:01 -0200

    Commit

---
+++ src/main/java/com/mycompany/mavenproject1/CadastroObrasMB.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Kamila
+ */
+public class CadastroObrasMB {
+    
+    public String novaObra(){
+        return "cadobras";
+    }
+}
